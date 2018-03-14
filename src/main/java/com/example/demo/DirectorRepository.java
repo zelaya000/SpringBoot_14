1fe0b49357a78816675d040191bfd9567dd698b3
zelaya <victorzelaya9462@gmail.com>
2018-03-14 18:26:38 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DirectorRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+import org.springframework.data.repository.CrudRepository;
+public interface DirectorRepository extends CrudRepository<Director, Long> {
+}
---
+++ src/main/java/com/example/demo/MovieRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+import org.springframework.data.repository.CrudRepository;
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+}
